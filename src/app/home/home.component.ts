47f72f4a51654f7a1ae231404d4f6754f5027358
AmonMcDuul <joranomic@hotmail.com>
2024-02-24 20:36:06 +0100

    next day sel logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,35 +9,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  gameLength = 30;
+  daysPassed = 0;
   balance = 100;
+  shield = 0;
+  weapon = 0;
   inventory = [
-    { name: 'Raktajino', quantity: 5 },
-    { name: 'Space biscuits', quantity: 3 },
-    { name: 'Warp core', quantity: 1 }
+    { name: 'Raktajino', price: 3, quantity: 5 },
+    { name: 'Space biscuits', price: 7, quantity: 3 },
+    { name: 'Warp core', price: 25, quantity: 1 }
   ];
   locations = ['Earth', 'Mars', 'Io', 'Sun station'];
   marketItems = [
-    { name: 'Space biscuits', price: 5 },
-    { name: 'Bolts', price: 10 },
-    { name: 'Iron', price: 15 }
+    { name: 'Space biscuits', price: 5, quantity: 5 },
+    { name: 'Bolts', price: 10, quantity: 5 },
+    { name: 'Iron', price: 15, quantity: 5 }
   ];
 
-  travel(location: string) {
-    console.log(`Traveling to ${location}`);
+  EndGame(){
+    console.log('Ended game');
   }
 
-  buy(item: { name: string; price: number }) {
+  NextDay(){
+    this.daysPassed++;
+    if(this.daysPassed >= this.gameLength){
+      this.EndGame();
+    }
+  }
+
+  Travel(location: string) {
+    console.log(`Traveling to ${location}`);
+    this.NextDay();
+  }
+
+  Buy(item: { name: string; price: number }) {
     if (this.balance >= item.price) {
       this.balance -= item.price;
       const inventoryItem = this.inventory.find(i => i.name === item.name);
-      if (inventoryItem) {
+      const marketItem = this.marketItems.find(i => i.name === item.name);
+      if (inventoryItem && marketItem) {
+        marketItem.quantity--;
         inventoryItem.quantity++;
       } else {
-        this.inventory.push({ name: item.name, quantity: 1 });
+        this.inventory.push({ name: item.name, price: item.price, quantity: 1 });
       }
       console.log(`Bought ${item.name}`);
     } else {
       console.log('Not enough money');
     }
+  }
+
+  Sell(item: { name: string; price: number }){
+    const marketItem = this.marketItems.find(i => i.name === item.name);
+    const inventoryItem = this.inventory.find(i => i.name === item.name);
+    this.balance += item.price;
+    if (inventoryItem && marketItem) {
+      inventoryItem.quantity--;
+      marketItem.quantity++;
+    } else {
+      this.marketItems.push({ name: item.name, price: item.price, quantity: 1 });
+    }
+    console.log(`Sold ${item.name}`);
   }
 }
