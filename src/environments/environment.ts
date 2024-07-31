b01a0c3d1b6015297b4d9460bc3f0c502281ea68
Joran Schols <schols.joran@gmail.com>
2024-07-31 17:12:17 +0200

    apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://js-backend-div.azurewebsites.net'
-    apiUrl: 'https://localhost:7104'
+    apiUrl: 'https://js-backend-div.azurewebsites.net'
+    // apiUrl: 'https://localhost:7104'
   };
